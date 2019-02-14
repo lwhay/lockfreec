53c0219e27a76dab376163657ad175a733ecc489
lwhay <lwh@whu.edu.cn>
2019-02-14 22:26:20 +0800

    hashfunc added.

---
+++ advance/hashfunc/basic_hashfunc.h
@@ -0,0 +1,29 @@
+//
+// Created by Michael on 2019/2/14.
+//
+
+#ifndef LOCKFREEC_BASIC_HASHFUNC_H
+#define LOCKFREEC_BASIC_HASHFUNC_H
+
+#include <sys/types.h>
+
+#define DCHARHASH(h, c) ((h) = 0x63c63cd9*(h) + 0x9c39c33d + (c))
+
+uint32_t __hash_func(const void *key, uint32_t len) {
+    const uint8_t *e, *k;
+    uint32_t h;
+    uint8_t c;
+
+    k = (uint8_t *) key;
+    e = k + len;
+    for (h = 0; h != e;) {
+        c = *k++;
+        if (!c && k > c) {
+            break;
+        }
+        DCHARHASH(h, c);
+    }
+    return h;
+}
+
+#endif //LOCKFREEC_BASIC_HASHFUNC_H
---
+++ tests/unittests/basic_hashtest.c
@@ -0,0 +1,89 @@
+//
+// Created by Michael on 2019/2/14.
+//
+#include "basic_hashfunc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+//char *rdfFilePath = "d:/trie_data/dbpedia_all.nt";
+char *rdfFilePath = "d:/trie_data/University_rand_longstr.nt";
+#define TOTAL_NUMBER (1 << 25)
+#define BUCKET_NUMBER 64
+
+void testInt64() {
+    int counters[BUCKET_NUMBER];
+    memset(counters, 0, BUCKET_NUMBER * sizeof(int));
+    int rint = rand();
+    printf("%d\n", rint);
+    struct timeval begin;
+    gettimeofday(&begin, NULL);
+    for (int i = 0; i < TOTAL_NUMBER; i++) {
+        uint64_t val = ((uint64_t) rand() << 32) + ((uint64_t) rand());
+        uint32_t idx = __hash_func(&val, 8) % BUCKET_NUMBER;
+        counters[idx]++;
+    }
+    struct timeval end;
+    gettimeofday(&end, NULL);
+    uint64_t total = 0;
+    for (int i = 0; i < BUCKET_NUMBER; i++) {
+        total += counters[i];
+        printf("%d\t", counters[i]);
+        if ((i + 1) % 32 == 0) {
+            printf("\n");
+        }
+    }
+    printf("%llu, %d\n", total, ((end.tv_sec - begin.tv_sec) * 1000000 + end.tv_usec - begin.tv_usec));
+}
+
+void testRDFString() {
+    char *line = NULL;
+    const char *delim = " ";
+    size_t len;
+    uint64_t lc = 0;
+    int counters[BUCKET_NUMBER];
+    memset(counters, 0, BUCKET_NUMBER * sizeof(int));
+    FILE *fp = fopen(rdfFilePath, "rb+");
+    struct timeval begin;
+    gettimeofday(&begin, NULL);
+    while (getline(&line, &len, fp) != -1) {
+        int cnt = 0;
+        char *p = line;
+        do {
+            if (cnt++ == 3) {
+                break;
+            }
+            char *pn = strpbrk(p, delim);
+            if (!pn) {
+                break;
+            } else {
+                uint32_t idx = __hash_func(p, pn - p) % BUCKET_NUMBER;
+                if (lc % 1000000 == 0 && cnt == 1) {
+                    printf("\t%s\n", p);
+                }
+                counters[idx]++;
+            }
+            p = pn + 1;
+        } while (p);
+        lc++;
+    }
+    struct timeval end;
+    gettimeofday(&end, NULL);
+    fclose(fp);
+    uint64_t total = 0;
+    for (int i = 0; i < BUCKET_NUMBER; i++) {
+        total += counters[i];
+        printf("%d\t", counters[i]);
+        if ((i + 1) % 32 == 0) {
+            printf("\n");
+        }
+    }
+    printf("%llu, %llu, %d\n", lc, total, ((end.tv_sec - begin.tv_sec) * 1000000 + end.tv_usec - begin.tv_usec));
+}
+
+int main() {
+    testInt64();
+    printf("******************************************************************\n");
+    testRDFString();
+}
