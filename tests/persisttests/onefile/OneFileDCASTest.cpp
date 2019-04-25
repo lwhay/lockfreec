952b0978652f090ffe4adedfc48fa3f09b5f1d41
Michael <lwhaywhu@gmail.com>
2019-04-25 22:45:43 +0800

    Revise testcase of oflf

--- tests/persisttests/onefile/OneFileDCASTest.cpp
+++ tests/persisttests/onefile/OneFileDCASTest.cpp
@@ -17,16 +17,22 @@ stringstream *output;
 
 typedef unsigned long long int atom_t;
 
-#define TOTAL_LOAD  (1 << 16)
-#define THREAD_NUM  1
+#define PRINT_INFO  0
+
 #define TEST_CAS    0
 #define TEST_DCAS   1
 #define TEST_OFDCAS 0
+
+#define TOTAL_LOAD  (1 << 20)
+#define PRINT_TICK  (1 << 4)
+#define THREAD_NUM  2
 #define LQ_POINTER  0
 #define LQ_COUNTER  1
 #define LQ_PAC_SIZE 2
 
 atom_t total_opers = 0;
+
+atom_t total_tries = 0;
 
 atom_t global_points[LQ_PAC_SIZE];
 
@@ -48,7 +54,7 @@ void initPayload() {
         local_others[i][LQ_POINTER] = (atom_t) ((i + 1) % THREAD_NUM);
 #elif TEST_OFDCAS
         local_others[i][LQ_POINTER] = (atom_t) ((i + 1) % THREAD_NUM);
-        local_others[i][LQ_POINTER] = (atom_t) ((i + 1) % THREAD_NUM);
+        local_others[i][LQ_COUNTER] = (atom_t) ((i + 1) % THREAD_NUM);
 #endif
         cout << "i " << i << " " << local_points[i][LQ_POINTER] << " " << local_others[i][LQ_POINTER] << endl;
     }
@@ -73,11 +79,17 @@ void *dcasWorker(void *args) {
 #elif TEST_DCAS
             cas_result = abstraction_dcas(global_points, local_points[tid], local_others[tid]);
 #elif TEST_OFDCAS
-            cas_result = DCAS(global_points, local_points[tid][LQ_POINTER], local_points[tid][LQ_COUNTER],
+            cas_result = DCAS(global_points, global_points[LQ_POINTER], global_points[LQ_COUNTER],
                               local_others[tid][LQ_POINTER], local_others[tid][LQ_COUNTER]);
 #endif
-            cout << tid << " " << self_tries << " " << global_points[LQ_POINTER] << " " << local_points[tid][LQ_POINTER]
-                 << " " << local_others[tid][LQ_POINTER] << " " << (atom_t) cas_result << endl;
+#if PRINT_INFO
+            if (TEST_CAS && (cas_result != local_points[tid][LQ_POINTER]) || cas_result == 0) {
+                cout << tid << " " << self_tries << " " << global_points[LQ_POINTER] << " " << global_points[LQ_COUNTER]
+                     << " " << local_points[tid][LQ_POINTER] << " " << local_points[tid][LQ_COUNTER] << " "
+                     << local_others[tid][LQ_POINTER] << " " << local_others[tid][LQ_COUNTER] << " "
+                     << (atom_t) cas_result << endl;
+            }
+#endif
             self_tries++;
 #if TEST_CAS
             // For __sync_val_compare_and_swap, we cannot guarantee cas_result to be changed by the current thread.
@@ -90,20 +102,27 @@ void *dcasWorker(void *args) {
 #elif TEST_DCAS
             // For dcas, cas_result denotes whether an exchange operation has been sucessfully enforced by the current thread.
         } while (cas_result == 0);
-        local_points[tid][LQ_POINTER] = (local_points[tid][LQ_POINTER] + 1) % THREAD_NUM;
-        local_points[tid][LQ_COUNTER] = (local_points[tid][LQ_COUNTER] + 1) % THREAD_NUM;
-        local_others[tid][LQ_POINTER] = (local_others[tid][LQ_POINTER] + 1) % THREAD_NUM;
-        local_others[tid][LQ_COUNTER] = (local_others[tid][LQ_COUNTER] + 1) % THREAD_NUM;
+        local_points[tid][LQ_POINTER] += THREAD_NUM;//= (local_points[tid][LQ_POINTER] + 1) % THREAD_NUM;
+        local_points[tid][LQ_COUNTER] += THREAD_NUM;//= (local_points[tid][LQ_COUNTER] + 1) % THREAD_NUM;
+        local_others[tid][LQ_POINTER] += THREAD_NUM;//= (local_others[tid][LQ_POINTER] + 1) % THREAD_NUM;
+        local_others[tid][LQ_COUNTER] += THREAD_NUM;//= (local_others[tid][LQ_COUNTER] + 1) % THREAD_NUM;
 #elif TEST_OFDCAS
             // For dcas, cas_result denotes whether an exchange operation has been sucessfully enforced by the current thread.
         } while (cas_result == 0);
-        local_points[tid][LQ_POINTER] = (local_points[tid][LQ_POINTER] + 1) % THREAD_NUM;
-        local_points[tid][LQ_COUNTER] = (local_points[tid][LQ_COUNTER] + 1) % THREAD_NUM;
-        local_others[tid][LQ_POINTER] = (local_others[tid][LQ_POINTER] + 1) % THREAD_NUM;
-        local_others[tid][LQ_COUNTER] = (local_others[tid][LQ_COUNTER] + 1) % THREAD_NUM;
+        local_points[tid][LQ_POINTER] += THREAD_NUM;//= (local_points[tid][LQ_POINTER] + 1) % THREAD_NUM;
+        local_points[tid][LQ_COUNTER] += THREAD_NUM;//= (local_points[tid][LQ_COUNTER] + 1) % THREAD_NUM;
+        local_others[tid][LQ_POINTER] += THREAD_NUM;//= (local_others[tid][LQ_POINTER] + 1) % THREAD_NUM;
+        local_others[tid][LQ_COUNTER] += THREAD_NUM;//= (local_others[tid][LQ_COUNTER] + 1) % THREAD_NUM;
 #endif
+#if PRINT_INFO
+        if (self_opers % (TOTAL_LOAD / THREAD_NUM / PRINT_TICK) == 0) {
+            cout << "\t" << global_points[LQ_COUNTER] << "<->" << global_points[LQ_POINTER] << endl;
+        }
+#endif
+        self_opers++;
     }
     __sync_fetch_and_add(&total_opers, self_opers);
+    __sync_fetch_and_add(&total_tries, self_tries);
     output[tid] << tid << ": " << self_opers << " " << self_tries << " " << tracer.getRunTime();
 }
 
@@ -123,7 +142,8 @@ void testDCAS() {
         string outstr = output[i].str();
         cout << outstr << endl;
     }
-    cout << total_opers << " " << tracer.getRunTime() << endl;
+    cout << "opers: " << total_opers << " tries: " << total_tries << " time: " << tracer.getRunTime() << " global: "
+         << global_points[LQ_POINTER] << "<->" << global_points[LQ_COUNTER] << endl;
     delete[] output;
 }
 
