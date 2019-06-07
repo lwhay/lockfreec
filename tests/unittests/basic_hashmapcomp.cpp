bcf61dda136f9756c3b6c2953626b4a4ba7aaf0b
lwhay <lwh@whu.edu.cn>
2019-06-08 02:56:24 +0800

    compare unordered and std maps.

--- tests/unittests/basic_hashmapcomp.cpp
+++ tests/unittests/basic_hashmapcomp.cpp
@@ -9,28 +9,52 @@
 
 using namespace std;
 
+template<typename T>
 void unordered_map_func() {
     Tracer tracer;
-    tracer.startTime();
-    unordered_map<int, int> map;
-    for (int i = 0; i < (1 << 22); i++) {
-        map.insert(make_pair(i, i));
+    for (int p = 20; p <= 26; p++) {
+        unordered_map<T, T> map;
+        tracer.startTime();
+        for (int i = 0; i < (1 << p); i++) {
+            map.insert(make_pair(static_cast<T>(i), static_cast<T>(i)));
+        }
+        long elipsed = tracer.getRunTime();
+        cout << "Uod Map: " << (1 << p) << " time: " << elipsed << " tpt: " << (double) (1 << p) / elipsed << endl;
     }
-    cout << "Unordered Map: " << tracer.getRunTime() << endl;
 }
 
+template<typename T>
 void std_map_func() {
     Tracer tracer;
-    tracer.startTime();
-    map<int, int> map;
-    for (int i = 0; i < (1 << 22); i++) {
-        map.insert(make_pair(i, i));
+    for (int p = 20; p <= 26; p++) {
+        map<T, T> map;
+        tracer.startTime();
+        for (int i = 0; i < (1 << p); i++) {
+            map.insert(make_pair(static_cast<T>(i), static_cast<T>(i)));
+        }
+        long elipsed = tracer.getRunTime();
+        cout << "Std Map: " << (1 << p) << " time: " << elipsed << " tpt: " << (double) (1 << p) / elipsed << endl;
     }
-    cout << "Unordered Map: " << tracer.getRunTime() << endl;
 }
 
 int main(int argc, char **argv) {
-    unordered_map_func();
-    std_map_func();
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@ int @@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    for (int r = 0; r < 1; r++) {
+        unordered_map_func<int>();
+        std_map_func<int>();
+        cout << "*****************************************************" << endl;
+    }
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@ long @@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    for (int r = 0; r < 1; r++) {
+        unordered_map_func<long>();
+        std_map_func<long>();
+        cout << "*****************************************************" << endl;
+    }
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@ double @@@@@@@@@@@@@@@@@@@@@@@@" << endl;
+    for (int r = 0; r < 1; r++) {
+        unordered_map_func<double>();
+        std_map_func<double>();
+        cout << "*****************************************************" << endl;
+    }
     return 0;
 }
