1041c8cb82bbdcc4d56b556f5608dba0b78c4573
lwhay <lwh@whu.edu.cn>
2019-06-13 02:56:10 +0800

    enable supermalloc test cases.

--- memory/futex_mutex.h
+++ memory/futex_mutex.h
@@ -12,18 +12,15 @@ extern "C" {
 
 // See futex_mutex.cc for the meaning of these fields.
 typedef struct futex_mutex_s {
-    volatile int locked;
-    volatile uint64_t n_locking;
-    volatile uint64_t n_waiting;
-    pthread_mutex_t mutex;
-    pthread_cond_t locking, waiting;
+    int lock __attribute__((aligned(64)));
+    int hold;
 } futex_mutex_t;
 
-#define FUTEX_MUTEX_INITIALIZER {0,0,0,PTHREAD_MUTEX_INITIALIZER,PTHREAD_COND_INITIALIZER,PTHREAD_COND_INITIALIZER}
+#define FUTEX_MUTEX_INITIALIZER {0,0}
 int futex_mutex_lock(futex_mutex_t *m); // return 0 if it's a fast lock, 1 if it's slow.
 void futex_mutex_unlock(futex_mutex_t *m);
 int futex_mutex_subscribe(futex_mutex_t *m); // (return 0 if it is unlocked)
-int futex_mutex_wait(futex_mutex_t *m); // return true if it was a long wait
+int futex_mutex_hold(futex_mutex_t *m); // return true if it was a long wait
 
 #ifdef TESTING
 void test_futex();
