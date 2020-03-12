575e5f6891c5304a5c78422ac1e2a692f8927520
lwhay <lwh@whu.edu.cn>
2020-03-12 06:28:17 -0700

    duplicate keys on bwtree

--- tests/btree/bwtree_unittest.cpp
+++ tests/btree/bwtree_unittest.cpp
@@ -42,8 +42,9 @@ public:
 
 uint64_t total_count = (1llu << 26);
 uint64_t thread_number = 1;
+uint64_t duplicate = (1 << 3);
 
-void SingleTest() {
+void SingleUniqueTest() {
     BwTree<uint64_t, uint64_t> *tree = new BwTree<uint64_t, uint64_t>();
     tree->UpdateThreadLocal(1);
     tree->AssignGCID(0);
@@ -60,6 +61,67 @@ void SingleTest() {
     }
     std::cout << total_count << " " << tracer.fetchTime() << " " << ((double) total_count / tracer.fetchTime())
               << std::endl;
+
+    tracer.startTime();
+    uint64_t correct = 0;
+    for (int i = 0; i < total_count; i++) {
+        std::vector<uint64_t> ret;
+        tree->GetValue(i, ret);
+        if (ret[0] == i) correct++;
+    }
+    std::cout << total_count << " " << correct << " " << tracer.fetchTime() << " "
+              << ((double) total_count / tracer.fetchTime()) << std::endl;
+
+    tracer.startTime();
+    correct = 0;
+    for (int i = total_count - 1; i >= 0; i--) {
+        std::vector<uint64_t> ret;
+        tree->GetValue(i, ret);
+        if (ret[0] == i) correct++;
+    }
+    std::cout << total_count << " " << correct << " " << tracer.fetchTime() << " "
+              << ((double) total_count / tracer.fetchTime()) << std::endl;
+    delete tree;
+}
+
+void SingleDuplicateTest() {
+    BwTree<uint64_t, uint64_t> *tree = new BwTree<uint64_t, uint64_t>();
+    tree->UpdateThreadLocal(1);
+    tree->AssignGCID(0);
+    Tracer tracer;
+    tracer.startTime();
+    for (int i = 0; i < total_count / duplicate; i++) {
+        for (int j = 0; j < duplicate; j++) tree->Insert(i, i + j);
+        Tracer it;
+        if (i % 10000000 == 0) {
+            it.startTime();
+            tree->PerformGC(0);
+            std::cout << "\t" << it.getRunTime() << std::endl;
+        }
+    }
+    std::cout << total_count << " " << tracer.fetchTime() << " " << ((double) total_count / tracer.fetchTime())
+              << std::endl;
+
+    tracer.startTime();
+    uint64_t correct = 0;
+    for (int i = 0; i < total_count / duplicate; i++) {
+        std::vector<uint64_t> ret;
+        tree->GetValue(i, ret);
+        for (int j = 0; j < duplicate; j++) if (ret[j] == (duplicate + i - j - 1)) correct++;
+        if (i == 1000) for (int j = 0; j < duplicate; j++) std::cout << "\t" << ret[j] << std::endl;
+    }
+    std::cout << total_count << " " << correct << " " << tracer.fetchTime() << " "
+              << ((double) total_count / tracer.fetchTime()) << std::endl;
+
+    tracer.startTime();
+    correct = 0;
+    for (int i = total_count / duplicate - 1; i >= 0; i--) {
+        std::vector<uint64_t> ret;
+        tree->GetValue(i, ret);
+        for (int j = 0; j < duplicate; j++) if (ret[j] == (duplicate + i - j - 1)) correct++;
+    }
+    std::cout << total_count << " " << correct << " " << tracer.fetchTime() << " "
+              << ((double) total_count / tracer.fetchTime()) << std::endl;
     delete tree;
 }
 
@@ -81,7 +143,8 @@ void MultiTest() {
 }
 
 int main(int argc, char **argv) {
-    SingleTest();
+    SingleUniqueTest();
+    SingleDuplicateTest();
     //MultiTest();
     return 0;
 }
